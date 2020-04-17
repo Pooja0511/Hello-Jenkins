1777c26bb487a405171e0f006b7d5c9b384cf762
Pooja0511 <muthagadurupooja1995@gmail.com>
2020-04-17 18:28:06 +0530

    adding Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main1(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1;i<=10;i++){
+			System.out.println("Hello --> "+i);
+			
+		}
+		
+	}
+
+}
